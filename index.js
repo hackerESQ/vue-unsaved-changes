bf025b321de31231f9725dfdba0f29140c8b19e9
hackerESQ <corey@coreyvarma.com>
2021-03-07 21:03:47 -0600

    format and more strongly check for custom event

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const pluginOptions = {
  *  Vue Component
  */
 
-const UnsavedChangesComponent = Vue.component('UnsavedChanges',{
+const UnsavedChangesComponent = Vue.component('UnsavedChanges', {
     name: "UnsavedChanges",
 
     render() {
@@ -42,26 +42,26 @@ const UnsavedChangesComponent = Vue.component('UnsavedChanges',{
 
                 // add listener if it has the listener attr
                 if (Object.keys(item.$attrs).indexOf(pluginOptions.listenAttr) > -1) {
-                    
-                    // reads custom event
+
+                    // reads custom event, if defined on the unsaved-listen attr
                     let event = 'input'
-                    if (item.$attrs[pluginOptions.listenAttr]) {
+                    if (!!item.$attrs[pluginOptions.listenAttr]) {
                         event = item.$attrs[pluginOptions.listenAttr]
                     }
-                    
+
                     // attaches listener
                     item.$on(event, (e) => {
                         // check if a conditional has been added
                         if (Object.keys(this.$attrs).includes(pluginOptions.conditionalAttr) && !!this.$attrs[pluginOptions.conditionalAttr]) {
                             // set dirty and emit event
                             this[pluginOptions.globalProperty].dirty(true)
-                            this.$emit('change-detected',true)
+                            this.$emit('change-detected', e)
                         }
                     })
-                }    
-                
+                }
+
                 // keep going down the node list?
-                if (Object.keys(item.$children).length > 0 ) {
+                if (Object.keys(item.$children).length > 0) {
                     this.addVueListeners(item.$children);
                 }
 
@@ -80,12 +80,12 @@ const UnsavedChangesComponent = Vue.component('UnsavedChanges',{
 
 export const UnsavedChanges = () => {
     if (instance) return instance;
-  
+
     instance = new Vue({
         data() {
-          return {
-            isDirty: false,
-          };
+            return {
+                isDirty: false,
+            };
         },
         render() {
             return this.$slots.default
@@ -94,10 +94,10 @@ export const UnsavedChanges = () => {
             UnsavedChanges: UnsavedChangesComponent
         },
         methods: {
-            dirty(val=null) {
+            dirty(val = null) {
                 if (val !== null) {
                     this.isDirty = val
-                    
+
                     // set native navigation middelware
                     window.onbeforeunload = () => {
                         return this.isDirty || null;
@@ -108,20 +108,20 @@ export const UnsavedChanges = () => {
             },
             middleware(next) {
                 // set vue router navigation middleware             
-                if (this[pluginOptions.globalProperty].dirty()) {                   
-                    if (window.confirm ('There are unsaved changes. Are you sure you want to leave?')) {
+                if (this[pluginOptions.globalProperty].dirty()) {
+                    if (window.confirm('There are unsaved changes. Are you sure you want to leave?')) {
                         this.isDirty = false
                         return next()
-                    } 
+                    }
                     return next(false)
                 }
                 return next()
             }
         }
-      });
-  
+    });
+
     return instance;
-  };
+};
 
 /**
  *  Vue Plugin Definition
